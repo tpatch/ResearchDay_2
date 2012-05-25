f46a59b9b65ed545a47a19fbdac84286607cd6c1
Trevor Patch <trevor.patch@gmail.com>
2012-05-25 15:41:37 -0500

    Script updates
    Added form field clearing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,10 +6,54 @@
 
 	window.API = {
 		init: function () {
-			this.method();
+			this.imagesize();
+			this.formhandler();
 		},
 
-		method: function () {}
+		imagesize: function () {
+			var artHeight = $(".mainphoto").height();
+			console.log(artHeight);
+
+			$("#submit").click(function(){
+				if($(".mainphoto")){
+					artHeight = $(".mainphoto").height();
+					console.log(artHeight);
+
+					$(".theart").css('height', artHeight);
+				}
+				else{
+					$("#submit").trigger('click');
+				}
+			});
+		},
+
+		formhandler: function() {
+			var tweetOrig = "Cray Cray",
+				picOrig = "Beautiful Sunset",
+				tweetVal = $("#tweets").val(),
+				picVal = $("#pic").val();
+
+			if(tweetOrig === tweetVal){
+				$("#tweets")
+					.css('color', '#acacac')
+					.focus(function(){
+						$(this)
+							.css('color', '#0776A0')
+							.val('');
+					});
+			};
+
+			if(picOrig === picVal){
+				$("#pic")
+					.css('color', '#acacac')
+					.focus(function(){
+						$(this)
+							.css('color', '#0776A0')
+							.val('');
+					});
+			};
+
+		}
 	};
 
 	$(window.document).ready(function () {
